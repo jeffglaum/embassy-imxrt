46e76b41086dbf467e5305a3fea9df02bc39ee42
Jeff Glaum <jeffglaum@live.com>
2025-03-07 17:37:25 -0800

    test enable systemview

--- examples/rt685s-evk/src/bin/gpio-blinky.rs
+++ examples/rt685s-evk/src/bin/gpio-blinky.rs
@@ -3,16 +3,42 @@
 
 extern crate embassy_imxrt_examples;
 
-use defmt::info;
+//use defmt::info;
 use embassy_executor::Spawner;
 use embassy_imxrt::gpio;
 use embassy_time::Timer;
+// N.B. systemview_target cannot be used at the same time as defmt_rtt.
+//use rtos_trace;
+use systemview_target::SystemView;
+
+static LOGGER: systemview_target::SystemView = systemview_target::SystemView::new();
+rtos_trace::global_trace! {SystemView}
+
+struct TraceInfo();
+
+impl rtos_trace::RtosTraceApplicationCallbacks for TraceInfo {
+    fn system_description() {}
+    fn sysclock() -> u32 {
+        64000000
+    }
+}
+rtos_trace::global_application_callbacks! {TraceInfo}
+
+#[inline(never)]
+#[no_mangle]
+unsafe fn _defmt_release() {}
+#[inline(never)]
+#[no_mangle]
+unsafe fn _defmt_write(bytes: &[u8]) {}
+#[inline(never)]
+#[no_mangle]
+unsafe fn _defmt_acquire() {}
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     let p = embassy_imxrt::init(Default::default());
 
-    info!("Initializing GPIO");
+    LOGGER.init();
 
     let mut led = gpio::Output::new(
         p.PIO0_26,
@@ -23,7 +49,7 @@ async fn main(_spawner: Spawner) {
     );
 
     loop {
-        info!("Toggling LED");
+        rtos_trace::trace::marker(13);
         led.toggle();
         Timer::after_millis(1000).await;
     }
